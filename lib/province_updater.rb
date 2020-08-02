428ba8704dcb32e6a0d98d682820b6e3deee52c4
= <amre.abdelhady@gmail.com>
2020-08-02 14:57:15 -0400

    CSV parsing is not prone to column changes

--- lib/province_updater.rb
+++ lib/province_updater.rb
@@ -5,29 +5,34 @@ class ProvinceUpdater
     file = CSV.new(open("https://health-infobase.canada.ca/src/data/covidLive/covid19.csv"))
     if file 
       rows = file.read
-      file_num_rows = rows.size - 1 #removing header from size
-      db_num_rows = Province.all.size
       
-      if file_num_rows > db_num_rows
+      nfile_body_rows = rows.size - 1 #removing header from size
+      ndb_rows = Province.all.size
+      
+      if nfile_body_rows > ndb_rows
+        first_row_to_enter = ndb_rows + 1 #shifting one down to exclude header
+        body = rows.slice(first_row_to_enter, nfile_body_rows)
         header = rows[0]
-        db_num_rows = [1, db_num_rows].max #handling data entry for the first time by not including header
-        body = rows.slice(db_num_rows, file_num_rows)
-        update_data(header,body)
+
+        hashed_body = hash_body(header, body)
+        update_data(hashed_body)
 
       else
         puts "No updates"
       end
-
     end
-    
   end
 
-  def update_data(header,body)
+  def hash_body(header,body)
+    body.map{|line| header.zip(line).to_h}
+  end
+
+  def update_data(hashed_body)
     failed = []
-    body.each do |line|
+    hashed_body.each do |line|
       province = Province.new
-      header.each_with_index do |key,i|
-        province[key] = line[i]
+      Province.column_names.each do |key|
+        province[key] = line[key]
       end
       if province.save
         puts "#{province.prname} saved"
@@ -35,6 +40,6 @@ class ProvinceUpdater
         failed << {name: province.prname, date: province.date, error: province.errors.messages}
       end
     end
-    puts "failed:#{failed.size} rows: #{failed}"
+    puts "failed:#{failed.size} rows: #{failed}" if failed.size > 0
   end
 end
